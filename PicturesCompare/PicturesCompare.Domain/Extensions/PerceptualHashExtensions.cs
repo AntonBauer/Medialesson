7c77b1ad87084d1492e90c93becf3e8bb2df71ad
Anton Bauer <abton.bauer@gmail.com>
2021-10-02 18:17:25 +0200

    Implemented perceptual hash

--- PicturesCompare/PicturesCompare.Domain/Extensions/PerceptualHashExtensions.cs
+++ PicturesCompare/PicturesCompare.Domain/Extensions/PerceptualHashExtensions.cs
@@ -3,18 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
 
 namespace PicturesCompare.Domain.Extensions
 {
     internal static class PerceptualHashExtensions
     {
-        public static Image<Rgb24> ApplyDcsPerRows(this Image<Rgb24> image) => image;
-
-        public static Image<Rgb24> ApplyDcsPerColumns(this Image<Rgb24> image) => image;
-
-        public static void ApplyDcs()
+        // https://content-blockchain.org/research/testing-different-image-hash-functions/
+        // Perceptual Hash
+        public static Image<Rgb24> ApplyDcsPerRows(this Image<Rgb24> image)
         {
+            image.Mutate(ctx => ctx.ProcessPixelRowsAsVector4(row =>
+            {
+                // ReSharper disable InconsistentNaming
+                var N = row.Length;
+                // ReSharper restore InconsistentNaming
+                
+                for (var i = 0; i < row.Length; i++)
+                {
+                    var k = i;
+                    var value = (float)Enumerable
+                       .Range(0, row.Length - 1)
+                       .Sum(n => 2 * n * Math.Cos(Math.PI * k * (2 * n + 1) / N));
+                    row[i].W = value;
+                    row[i].X = value;
+                    row[i].Y = value;
+                    row[i].Z = value;
+                }
+            }));
             
+            return image;
+        }
+
+        public static Image<Rgb24> ApplyDcsPerColumns(this Image<Rgb24> image)
+        {
+            image.Mutate(ctx => ctx.Rotate(RotateMode.Rotate90));
+            image = image.ApplyDcsPerRows();
+            image.Mutate(ctx => ctx.Rotate(RotateMode.Rotate270));
+            
+            return image;
         }
 
         public static int CalcPerceptualHash(this Image<Rgb24> image)
